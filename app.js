299ef771df0f7ac80eab053f31db1b2f01eae1e0
samuel <wonho.jung92@gmail.com>
2023-09-04 20:53:13 -0400

    learned about the fs

--- app.js
+++ app.js
@@ -35,3 +35,13 @@ const fs = require("fs");
 //     console.log("folder deleted");
 //   });
 // }
+
+// deleting files
+// if (fs.existsSync("./docs/deleteme.txt")) {
+//   fs.unlink("./docs/deleteme.txt", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("file deleted");
+//   });
+// }
