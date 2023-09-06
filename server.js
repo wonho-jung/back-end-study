5208379fd6447c445551649d0f285d21bef7ad5f
samuel <wonho.jung92@gmail.com>
2023-09-05 21:36:45 -0400

    basic server router

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  // console.log(req.url, req.method);
+  res.setHeader("Content-Type", "text/html");
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("sorry, we can't find the file");
+    } else {
+      res.end(data);
+    }
+  });
+
+  // fs.readFile("./views/index.html", (err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //     res.end("sorry, we can't find the file");
+  //   } else {
+  //     //   res.write(data);
+  //     //   res.write("<p>hello, element</p>");
+
+  //     res.end(data);
+  //   }
+  // });
+});
+
+server.listen(3000, () => {
+  console.log("Server is running...");
+});
