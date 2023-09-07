95fd4b57cec463f2dd53a6d097369f2c41ad1fca
samuel <wonho.jung92@gmail.com>
2023-09-06 23:47:14 -0400

    ejs

--- express.js
+++ express.js
@@ -3,20 +3,32 @@ const express = require("express");
 // express app
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
+
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
-// redirects
 app.get("/about-me", (req, res) => {
   res.redirect("/about");
 });
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
+
+// // redirects
+// app.get("/about-me", (req, res) => {
+//   res.redirect("/about");
+// });
+// // 404 page
+// app.use((req, res) => {
+//   res.status(404).sendFile("./views/404.html", { root: __dirname });
+// });
